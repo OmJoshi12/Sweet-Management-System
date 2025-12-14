bde13f54bf45ab7746ff3d199de87cb47a57ce1e
OmJoshi12 <joshiom907@gmail.com>
2025-12-14 17:37:35 +0530

    Remove specified files from repository and ignore them

--- start-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Om  Joshi\OneDrive\Desktop\Sweet Management System\backend"
-npm start
-pause
--- start-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Om  Joshi\OneDrive\Desktop\Sweet Management System\frontend"
-npm start
-pause
